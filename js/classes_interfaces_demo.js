d92ae336eefc1b32d0128629e7d0da96bd9f08de
David Spina <David Spina>
2017-02-18 01:39:46 -0500

    Modified callback and extended interface.

--- js/classes_interfaces_demo.js
+++ js/classes_interfaces_demo.js
@@ -4,24 +4,55 @@ var CarEngine = (function () {
     function CarEngine(horsePower, engineType) {
         this.horsePower = horsePower;
         this.engineType = engineType;
+        this.oilLevel = 100;
     }
+    Object.defineProperty(CarEngine.prototype, "OilLevel", {
+        get: function () {
+            return this.oilLevel;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    CarEngine.prototype.useOil = function (amount) {
+        if (amount != undefined) {
+            this.oilLevel -= amount;
+        }
+        else {
+            this.oilLevel--;
+        }
+    };
+    CarEngine.prototype.changeOil = function () {
+        this.oilLevel = 100;
+    };
     CarEngine.prototype.start = function (callback) {
         var _this = this;
         setTimeout(function () {
             callback(true, _this.engineType);
         }, 1000);
+        this.useOil(10);
     };
     CarEngine.prototype.stop = function (callback) {
         var _this = this;
         setTimeout(function () {
-            callback(true, _this.engineType);
+            callback(false, _this.engineType);
         }, 1000);
     };
     return CarEngine;
 }());
+var myEngineFunc = function (status, type) {
+    var startOrStop = status ? "started" : "stopped";
+    console.log(type + " engine started - " + startOrStop);
+};
 var engine1 = new CarEngine(120, "V8");
 console.log("Engine is initialized...");
-engine1.start(function (status, type) {
-    console.log(type + " is started.");
-});
+// alternative way of handling the callback
+// engine1.start( (status: boolean, type: string) => {
+//     console.log(type + " is started.");
+// });
+engine1.start(myEngineFunc);
+console.log("Oil level: " + engine1.OilLevel.toString());
+console.log('Replenishing oil...');
+engine1.changeOil();
+console.log("Oil level: " + engine1.OilLevel.toString());
+engine1.stop(myEngineFunc);
 //# sourceMappingURL=classes_interfaces_demo.js.map
--- ts/classes_interfaces_demo.ts
+++ ts/classes_interfaces_demo.ts
@@ -6,6 +6,12 @@ interface IEngine{
     stop(callback: (stopStatus: boolean, engineType: string) => void) : void; 
 }
 
+interface IOilEngine{
+    oilLevel: number;
+    changeOil() : void;
+    useOil(amount?: number): void;
+}
+
 interface IAutoOptions{
     engine: IEngine;
     blasePrice: number;
@@ -15,30 +21,66 @@ interface IAutoOptions{
     year?: number;
 }
 
-class CarEngine implements IEngine{
+// extending an interface
+interface ITruckOptions extends IAutoOptions{
+    bedLength?: string;
+    fourByFour: boolean;
+}
 
+class CarEngine implements IOilEngine{
+    oilLevel: number;
     constructor(public horsePower: number, public engineType: string){
-    
+        this.oilLevel = 100;
+    }
+
+    get OilLevel(){
+        return this.oilLevel;
+    }
+
+    useOil(amount?: number){
+        if(amount != undefined){
+            this.oilLevel -= amount;
+        }
+        else{
+            this.oilLevel--;
+        }
+    }
+
+    changeOil(){
+        this.oilLevel = 100;
     }
 
     start(callback: (startStatus: boolean, engineType: string) => void){
         setTimeout(() => {
                 callback(true, this.engineType);
         }, 1000);
+        this.useOil(10);
     }
 
     
     stop(callback: (stopStatus: boolean, engineType: string) => void){
         setTimeout(() => {
-                callback(true, this.engineType);
+                callback(false, this.engineType);
         }, 1000);
+
     }
 }
 
 
+var myEngineFunc = function(status: boolean, type:string){
+    var startOrStop = status ? "started" : "stopped";
+    console.log(type + " engine started - " + startOrStop);
+};
+
 var engine1 = new CarEngine(120, "V8");
 console.log("Engine is initialized...");
-engine1.start( (status: boolean, type: string) => {
-    console.log(type + " is started.");
-});
-
+// alternative way of handling the callback
+// engine1.start( (status: boolean, type: string) => {
+//     console.log(type + " is started.");
+// });
+engine1.start(myEngineFunc);
+console.log("Oil level: " + engine1.OilLevel.toString());
+console.log('Replenishing oil...');
+engine1.changeOil();
+console.log("Oil level: " + engine1.OilLevel.toString());
+engine1.stop(myEngineFunc);
